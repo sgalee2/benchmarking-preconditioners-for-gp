eb9b923d88b4cff644c8ebc58c4ab175b3afc17f
AJL-1998 <73225992+AJL-1998@users.noreply.github.com>
2022-02-02 12:31:55 +0000

    Single solution precon lin_cg

--- preconditioner_tests/conjugate_gradients/__init__.py
+++ preconditioner_tests/conjugate_gradients/__init__.py
@@ -7,5 +7,5 @@ Created on Tue Feb  1 12:23:29 2022
 
 class conjugate_gradients:
     
-    max_error: float = 1.0
+    tol: float = 1.0
     max_its: int = 1000
--- preconditioner_tests/conjugate_gradients/linear_cg.py
+++ preconditioner_tests/conjugate_gradients/linear_cg.py
@@ -49,6 +49,8 @@ class linear_cg(conjugate_gradients):
             b: Tensor,
             x_0: Tensor = None,
             precon = None,
+            tol = 1e-8,
+            max_its = 1000
         ):
         """
         Parameters
@@ -70,15 +72,94 @@ class linear_cg(conjugate_gradients):
               List of solution and number of iterations
 
         """
+        #extract some parameters
+        b = b.clone()
         b_shape = b.shape
         n = b_shape[0]
         
-        
-        
-        
+        #ensure rhs is n x d array
+        if len(b_shape) == 1:
+            b.reshape(n, 1)
+            d = 1
+        else:
+            d = b_shape[1]
+            TypeError('We do not yet support multidimension problems')
+            
         if precon is None:
             precon = self.no_precon
-        return precon(A)
+        elif type(precon) == torch.Tensor:
+            precon = precon.matmul
+        elif not callable(precon):
+            TypeError('Preconditioner is not a tensor or callable.')
+            
+        work_vecs = torch.zeros(n,5)
+        
+        if x_0 is not None:
+            
+            work_vecs[:,1:2] = x_0
+            work_vecs[:,2:3] = b - (A @ x_0)
+            residual = torch.norm(work_vecs[:,2:3])
+            
+            work_vecs[:,4:5] = precon(work_vecs[:,2:3])
+            work_vecs[:,3:4] = work_vecs[:,4:5]
+            precon_res = work_vecs[:,2:3].T @ work_vecs[:,4:5]
+        
+        else:
+            
+            work_vecs[:,2:3] = b
+            residual = torch.norm(work_vecs[:,2:3])
+            work_vecs[:,4:5] = precon(work_vecs[:,2:3])
+            work_vecs[:,3:4] = work_vecs[:,4:5]
+            precon_res = work_vecs[:,2:3].T @ work_vecs[:,4:5]
+        
+        i = 0
+        while i < max_its and abs(residual.item()) > tol:
+            i += 1
+            work_vecs[:,0:1] = A @ work_vecs[:,3:4]
+            alpha = precon_res / (work_vecs[:,3:4].T @ work_vecs[:,0:1])
+            work_vecs[:,1:2] += alpha*work_vecs[:,3:4] #x = x + alpha*d
+            if i%50 == 0:
+                work_vecs[:, 2:3] = b - (A @ work_vecs[:, 1:2]) #force an extra MVP to combat round off errors
+            else:
+                work_vecs[:, 2:3] += - alpha*work_vecs[:, 0:1] #r = r - alpha*q
+            residual = torch.norm(work_vecs[:,2:3])
+            work_vecs[:,4:5] = precon(work_vecs[:,2:3])
+            new_res = work_vecs[:,2:3].T @ work_vecs[:,4:5]
+            beta = new_res  / precon_res
+            work_vecs[:,3:4] = work_vecs[:,4:5] + beta*work_vecs[:,3:4]
+            precon_res = new_res
+            
+        if abs(residual) <= tol:
+            return work_vecs[:,1:2], 1, i
+        else:
+            return work_vecs[:,1:2], 0, i
 
-x = torch.rand(size=[5])
-print(x.reshape([5,1]))
+n = 5
+X = torch.rand([n,n])
+A = X.T @ X + 0.0001*torch.eye(n)
+b = torch.rand([n,1])
+
+x_sol = torch.inverse(A) @ b
+
+vals, vecs = torch.linalg.eig(A)
+P = vecs[:, 0:5] @ torch.diag(vals[0:5]) @ vecs[:, 0:5].T
+precon = torch.inverse(P.real)
+
+cg_ = linear_cg()
+cg_run = cg_(A, b,)
+cg_run_p = cg_(A, b, precon=precon)
+
+print(x_sol)
+print(cg_run)
+print(cg_run_p)
+
+
+
+
+
+
+
+
+
+
+
